cf97424cb33c016cca5b699313827014e86ef745
Adi-Aiman <adiaimanramlan@gmail.com>
2023-12-24 09:03:43 +0800

    Fix table book design, id is now primary key. #3

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-#from flask_smorest import Api
+from flask_smorest import abort
 from flask_restful import Api
 from flask_swagger_ui import get_swaggerui_blueprint
-
+from sqlalchemy.exc import SQLAlchemyError
 
 
 
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://book_inventory_api_user:kDtYxkAwQOAn1t6F3fXLGZNqq5Rk0YCc@dpg-cm2it6ta73kc73eij540-a.singapore-postgres.render.com/book_inventory_api"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
-#app.config["API_TITLE"] = "Stores REST API"
+#app.config["API_TITLE"] = "Book Inventory REST API"
 #app.config["API_VERSION"] = "v1"
 #app.config["OPENAPI_VERSION"] = "3.0.3"
 #app.config["OPENAPI_URL_PREFIX"] = "/"
@@ -45,7 +45,8 @@ migrate = Migrate(app, db)
 class BooksModel(db.Model):
     __tablename__ = 'book'
 
-    isbn = db.Column(db.String(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
+    isbn = db.Column(db.String() ,unique=True, nullable=True)
     title = db.Column(db.String())
     author = db.Column(db.String())
     year_published = db.Column(db.Integer())
@@ -53,6 +54,7 @@ class BooksModel(db.Model):
     
 
     def __init__(self, isbn, title, author,year_published,genre):
+        
         self.isbn = isbn
         self.title = title
         self.author = author
@@ -76,10 +78,13 @@ def handle_books():
     if request.method == 'POST':
         if request.is_json:
             data = request.get_json()
-            new_book = BooksModel(isbn=data['isbn'],title=data['title'], author=data['author'], year_published=data['year_published'],genre=data['genre'])
+            new_book = BooksModel( isbn=data['isbn'],title=data['title'], author=data['author'], year_published=data['year_published'],genre=data['genre'])
 
-            db.session.add(new_book)
-            db.session.commit()
+            try:
+                db.session.add(new_book)
+                db.session.commit()
+            except SQLAlchemyError:
+                return abort(make_response({'message':'An Error Occured While Inserting The Book'},500))
 
             return {"message": f"book {new_book.title} has been created successfully."}
         else:
@@ -101,9 +106,9 @@ def handle_books():
 #GET method : return a specific book
 #PUT method : update a specific book
 #DELETE method : delete a specific book
-@app.route('/books/<book_isbn>', methods=['GET', 'PUT', 'DELETE'])
-def handle_book(book_isbn):
-    book = BooksModel.query.get_or_404(book_isbn)
+@app.route('/books/<book_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_book(book_id):
+    book = BooksModel.query.get_or_404(book_id)
 
     if request.method == 'GET':
         response = {
@@ -122,15 +127,20 @@ def handle_book(book_isbn):
         book.author = data['author']
         book.year_published = data['year_published']
         book.genre = data['genre']
-
-        db.session.add(book)
-        db.session.commit()
+        try:
+            db.session.add(book)
+            db.session.commit()
+        except SQLAlchemyError:
+                return abort(make_response({'message':'An Error Occured While Updating The Book'},500))
         
         return {"message": f"Book {book.title} successfully updated"}
 
     elif request.method == 'DELETE':
-        db.session.delete(book)
-        db.session.commit()
+        try:
+            db.session.delete(book)
+            db.session.commit()
+        except SQLAlchemyError:
+                return abort(make_response({'message':'An Error Occured While Deleting The Book'},500))
         
         return {"message": f"Book {book.title} successfully deleted."}
 
